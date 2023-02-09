704a2c5fb21544632fdcfe463991af72265da246
MarianaFigueiredoI <113534989+MarianaFigueiredoI@users.noreply.github.com>
2023-02-09 15:37:37 -0300

    Correção de bug na indentação do arquivo SQL.

--- modelagem relacional/steam.sql
+++ modelagem relacional/steam.sql
@@ -6,7 +6,7 @@ USE steam;
 
 -- Cria a tabela 'games'.
 CREATE TABLE games (
-	gam_id INT PRIMARY KEY,
+    gam_id INT PRIMARY KEY,
     gam_name VARCHAR(255) NOT NULL,
     gam_release_date DATE NOT NULL, 
     gam_developer VARCHAR(255) NOT NULL,
@@ -22,7 +22,7 @@ CREATE TABLE games (
 
 -- Cria a tabela 'media data'.
 CREATE TABLE media_data (
-	med_id INT PRIMARY KEY AUTO_INCREMENT,
+    med_id INT PRIMARY KEY AUTO_INCREMENT,
     med_game INT NOT NULL,
     med_header_image BLOB,
     med_screenshots BLOB,
@@ -33,19 +33,19 @@ CREATE TABLE media_data (
 
 -- Cria a tabela 'description data'.
 CREATE TABLE description_data (
-	des_id INT PRIMARY KEY AUTO_INCREMENT,
+    des_id INT PRIMARY KEY AUTO_INCREMENT,
     des_game INT NOT NULL,
-	des_detailed MEDIUMTEXT,
-	des_about_the_game TEXT,
+    des_detailed MEDIUMTEXT,
+    des_about_the_game TEXT,
     des_short_description TINYTEXT NOT NULL,
     FOREIGN KEY (des_game) REFERENCES games (gam_id)
 );
 
 -- Cria a tabela 'support info'.
 CREATE TABLE support_info (
-	sup_id  INT PRIMARY KEY AUTO_INCREMENT,
+    sup_id  INT PRIMARY KEY AUTO_INCREMENT,
     sup_game INT NOT NULL,
-	sup_website VARCHAR(255),
+    sup_website VARCHAR(255),
     sup_url VARCHAR(255),
     sup_email VARCHAR(255),
     FOREIGN KEY (sup_game) REFERENCES games (gam_id)
@@ -53,12 +53,12 @@ CREATE TABLE support_info (
 
 -- Cria a tabela 'requirements data'.
 CREATE TABLE requirements_data (
-	req_id INT PRIMARY KEY AUTO_INCREMENT,
+    req_id INT PRIMARY KEY AUTO_INCREMENT,
     req_game INT NOT NULL,
-	req_pc VARCHAR(255),
+    req_pc VARCHAR(255),
     req_mac VARCHAR(255),
     req_linux VARCHAR(255),
     req_minimum VARCHAR(255),
     req_recommended VARCHAR(255),
-	FOREIGN KEY (req_game) REFERENCES games (gam_id)
+    FOREIGN KEY (req_game) REFERENCES games (gam_id)
 );
